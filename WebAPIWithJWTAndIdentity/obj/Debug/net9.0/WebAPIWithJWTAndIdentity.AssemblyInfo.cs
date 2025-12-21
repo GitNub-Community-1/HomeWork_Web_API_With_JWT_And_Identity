858b5413d1845c73ecbc7b81ca5793008e44fa78
GitNub-Community-1 <maxik010020003@gmail.com>
2025-12-21 22:50:20 +0500

    Changed path of file git ignore for his correct working

--- WebAPIWithJWTAndIdentity/obj/Debug/net9.0/WebAPIWithJWTAndIdentity.AssemblyInfo.cs
+++ WebAPIWithJWTAndIdentity/obj/Debug/net9.0/WebAPIWithJWTAndIdentity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPIWithJWTAndIdentity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26acb7fe872a9468909a2e2f54b991b7f96552af")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPIWithJWTAndIdentity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPIWithJWTAndIdentity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
